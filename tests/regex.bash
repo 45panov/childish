8df20e3a038b77c476e0bd307b0e4755957f9993
45panov <khorkov.anton@gmail.com>
2024-07-09 13:36:03 +0300

    Count sum of durations of all finished sessions from 'last' output for current date. .gitignore added.

--- tests/regex.bash
+++ tests/regex.bash
@@ -6,13 +6,21 @@
 #  Dummy variables which are to be replaced with dynamic ones in main script.
 username="aspanov"
 procname="tty7"
-dayweek=$(echo $(LANG=en-us88591; date +"%a"))
-month=$(echo $(LANG=en-us88591; date +"%b"))
-day=14
 
-# Extract sessions' duration from dummy 'last' output (last.txt file). 
-str_of_timestamps=$(cat last.txt \
-    | grep -E "${username}.*${procname}.*${dayweek}.*${month} ${day}" \
+# Real date.
+#dayweek=$(echo $(LANG=en-us88591; date +"%a"))
+#month=$(echo $(LANG=en-us88591; date +"%b"))
+#day=$(echo $(LANG=en-us88591; date +"%e"))
+
+# Dummy date.
+dayweek="Wed"
+month="Jun"
+day=19
+
+
+# Extract finished sessions' duration from dummy 'last' output (last.txt file). 
+str_of_timestamps=$(last \
+    | grep -E "${username}[[:space:]]*${procname}.*${dayweek}[[:space:]]*${month}[[:space:]]*${day}" \
     | grep -oP "\([0-9][0-9]:[0-9][0-9]\)" \
     | tr -d '()'
 )
@@ -22,10 +30,16 @@ array_of_timestamps=(${str_of_timestamps})
 
  
 # Convert arrow of strings into arraow of integers.
-array_of_seconds=()
+array_of_minutes=()
 for ((i = 0; i < ${#array_of_timestamps[@]}; i++)); do
-    array_of_seconds[${i}]+=$(echo ${array_of_timestamps[${i}]} | awk -F: '{ print ($1 * 60) + $2  }')
-    echo ${array_of_seconds[${i}]}
+    array_of_minutes[${i}]+=$(echo ${array_of_timestamps[${i}]} | awk -F: '{ print ($1 * 60) + $2  }')
+#    echo ${array_of_minutes[${i}]}
 done
 
+# Count duration of all finished sessions.
+all_sess_duration=$(expr 0)
+for elem in "${array_of_minutes[@]}"; do
+    all_sess_duration=$(expr $all_sess_duration + $elem)
+done
 
+echo All sessions of ${month}, ${day} had lasted for  ${all_sess_duration} minutes.
