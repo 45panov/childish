82ebb7050d0d4d96e6695fe7295a009d1a7fb431
45panov <khorkov.anton@gmail.com>
2024-07-11 10:55:35 +0300

    Now counts duration of current session.

--- childi.sh
+++ childi.sh
@@ -1,16 +1,25 @@
-#!/bin/bash
+        #!/bin/bash
 
 . ~/childish/childish.conf
 
 # Exit script immediately if whoiam output is not in target_user.
 
 if ! [[ "${target_user[*]}" =~ `whoami` ]]; then
-    if [$production]; then
-       exit
-    else
-        echo "Exit Childish, no current user in \$target_user." 
-    fi
+    echo Exit, because no `whoami` in target_user
+    exit
 fi
+username="aspanov"
+procname="tty7"
+
+# Real date.
+dayweek=$(echo $(LANG=en-us88591; date +"%a"))
+month=$(echo $(LANG=en-us88591; date +"%b"))
+day=$(echo $(LANG=en-us88591; date +"%e"))
+
+# Dummy date.
+#dayweek="Wed"
+#month="Jun"
+#day=19
 
 
 # Get index of current user in target_user array.
@@ -23,5 +32,5 @@ done
 
 
 # Get allowed time from default_time array by user_index.
-allowed_time=${default_time[$user_index]}
+allowed_time=${default_time[${user_index}]}
 
---
+++ tests/time_delta.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+
+# Here we test regular expression which extracts durarion of
+# current sesssions from 'last' command output.
+
+#  Dummy variables which are to be replaced with dynamic ones in main script.
+username="aspanov"
+procname="tty7"
+
+# Real date.
+dayweek=$(echo $(LANG=en-us88591; date +"%a"))
+month=$(echo $(LANG=en-us88591; date +"%b"))
+day=$(echo $(LANG=en-us88591; date +"%e"))
+
+# Dummy date.
+#dayweek="Wed"
+#month="Jun"
+#day=19
+
+
+# Extract finished sessions' duration from dummy 'last' output (last.txt file). 
+start_of_current_session=$(last \
+    | grep -E "${username}[[:space:]]*${procname}.*${dayweek}[[:space:]]*${month}[[:space:]]*${day}" \
+    | grep -E gone \
+    | grep -oP "[0-9][0-9]:[0-9][0-9]" | awk -F: '{ print ($1 * 60) + $2  }' 
+)
+
+echo Current session started: ${start_of_current_session}
+current_time=$(date +%T)
+current_time=$(echo ${current_time:0:5} | awk -F: '{ print ($1 * 60) + $2  }')
+echo Current time: echo ${current_time}
+echo Corrent session is lasting for $(expr ${current_time} - ${start_of_current_session})
