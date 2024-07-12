1dba45b1ebf9c91253bb3aba3943a4cba3dd8508
45panov <khorkov.anton@gmail.com>
2024-07-12 12:58:33 +0300

    Main code finished

--- childi.sh
+++ childi.sh
@@ -1,4 +1,4 @@
-        #!/bin/bash
+#!/bin/bash
 
 # Import configuration.
 . ~/childish/childish.conf
@@ -17,7 +17,7 @@ username=`whoami`
 # Get index of current user in target_user array from 'childish.conf'.
 user_index=""
 for i in "${!target_user[@]}"; do
-    if [[ "${target_user[$i]}" == `whoami` ]]; then
+    if [[ "${target_user[$i]}" == "${username}" ]]; then
         user_index="${i}";
     fi
 done
@@ -39,7 +39,7 @@ day=$(echo $(LANG=en-us88591; date +"%e"))
 #month="Jun"
 #day=19
 
-# Extract finished sessions' duration from dummy 'last' output. 
+#` Extract finished sessions' duration from dummy 'last' output. 
 str_of_timestamps=$(last \
     | grep -E "${username}[[:space:]]*tty7.*${dayweek}[[:space:]]*${month}[[:space:]]*${day}" \
     | grep -oP "\([0-9][0-9]:[0-9][0-9]\)" \
@@ -58,11 +58,11 @@ for ((i = 0; i < ${#array_of_timestamps[@]}; i++)); do
 done
 
 # Count duration of all finished sessions.
-all_sess_duration=$(expr 0)
+finished_sess_duration=$(expr 0)
 for elem in "${array_of_minutes[@]}"; do
-    all_sess_duration=$(expr $all_sess_duration + $elem)
+    finished_sess_duration=$(expr ${all_sess_duration} + ${elem})
 done
-echo Finished sessions duration is ${all_sess_duration} minutes.
+echo Finished sessions duration is ${finished_sess_duration} minutes.
 
 
 
@@ -78,6 +78,20 @@ start_of_current_session=$(last \
 echo Current session started: ${start_of_current_session}
 current_time=$(date +%T)
 current_time=$(echo ${current_time:0:5} | awk -F: '{ print ($1 * 60) + $2  }')
-echo Current time: echo ${current_time}
-echo Corrent session is lasting for $(expr ${current_time} - ${start_of_current_session})
-echo Allowed time is ${allowed_time}
+echo Current time: ${current_time}
+current_sess_duration=$(expr ${current_time} - ${start_of_current_session})
+echo Current session is lasting for  ${current_sess_duration}
+all_sess_duration=$(expr ${finished_sess_duration} + ${current_sess_duration})
+echo All sessions\' duration  is ${all_sess_duration}
+diff=$(expr ${allowed_time} - ${current_sess_duration})
+echo Difference between allowed and elapsed time is ${diff} 
+
+if [[ "$diff" -le 0 && "$production" = true ]]; then
+    pkill -kill -u ${username}
+elif [[ "$diff" -le 0 && "$production" = false ]]; then
+    echo Production mode OFF. Allowed time is over.
+else
+    echo Time is not over
+    exit
+fi
+
